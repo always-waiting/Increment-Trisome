7984e616348a9200a06275716d23f50d52df40d0
bixichao <bixichao@berrygenomics.com>
2017-07-12 14:32:53 +0800

    Add cmd option for zscore ref production

--- statistic_zscore.py
+++ statistic_zscore.py
@@ -3,14 +3,30 @@
 import glob
 import pandas as pd
 import numpy as np
+import argparse
+
 """
 收集输入目录中所有samplename.zscore文件，获取PZ值(即最后一列)，计算均值，方差
 作为判断其他样本特性的参考集，
 """
 
-inputdir = "/home/bixichao/Projects/tmp/Increment/mapping/"
-ext = "zscore"
-retfile = "./zscore"
+parse = argparse.ArgumentParser(description="收集输入目录中所有samplename.zscore文件，获取PZ值(即最后1列)，计算均值，方差作为判断其他样本特性的参考集")
+parse.add_argument("--indir",'-idir',help="输入目录，存有.zscore文件",required=True)
+parse.add_argument("--bin",'-b',help="窗口中bin数", default=1,type=int)
+parse.add_argument("--male-list",'-ml',help="男性样本列表，为了处理性染色体",default="01-sample.male.list.txt", dest="malelist")
+parse.add_argument("--female-list",'-fl', help="女性样本列表",default="01-sample.female.list.txt",dest="femalelist")
+parse.add_argument("--ext","-e",help='查找文件的后缀,有一个%%d参数',default="b%d.zscore.txt")
+args = parse.parse_args()
+
+#inputdir = "/home/bixichao/Projects/tmp/Increment/mapping/"
+#ext = "zscore"
+#retfile = "./zscore"
+inputdir = args.indir
+win_bin = args.bin
+ext = args.ext%args.bin
+retfile = "zscore.b%d.txt"%args.bin
+malelist = args.malelist
+femalelist = args.femalelist
 chromlist = range(1,25)
 
 zscore_list = []
@@ -27,8 +43,8 @@ mean = zscore.mean(axis=0).astype(np.str)
 std = zscore.std().astype(np.str)
 
 # 处理X染色体
-malelist = "01-c-sample.male.list"
-femalelist = "01-c-sample.female.list"
+#malelist = "01-c-sample.male.list"
+#femalelist = "01-c-sample.female.list"
 maleindex = pd.read_csv(malelist,index_col=0, header=None).index
 femaleindex = pd.read_csv(femalelist,index_col=0, header=None).index
 
