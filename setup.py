f5c9e89182a9cd9ac103982fa98b9e3a96b52ca0
bixichao <bixichao@berrygenomics.com>
2017-08-14 11:03:05 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="berry_increment_trisome",
+    version="0.0.1",
+    description="The Method for xromate trisome detection",
+    author="bixichao",
+    author_email="bixichao@berrygenomics.com",
+    license='MIT',
+    packages=['berry_increment_trisome'],
+    zip_safe=False
+)
